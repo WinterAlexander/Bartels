62f4f0cf527afb2ae75e3828ebfa46e8019ea66b
Alexander <a.w1nter@hotmail.com>
2020-05-08 16:13:18 -0400

    Removed matlab prototype from the library

--- adaptive-rigidification/test_bartels.m
+++
@@ -1,5 +0,0 @@
-[V,T,F] = readMESH('data/meshes_mesh/coarser_bunny.mesh');
-
-disp('Mesh loaded');
-
-B = linear_tetmesh_dphi_dX(V, T)
--- adaptive-rigidification/volcontact/afun.m
+++
@@ -1,6 +0,0 @@
-function [Ax] = afun( x, mesh, h )
-% afun Evaluates ((1 - h * alpha0)M - (h alpha1 + h^2) K) x
-
-mesh.dp = x;
-df = computeForceDifferentials( mesh );
-Ax = (1 - mesh.alpha0*h) * mesh.M * x - (h*mesh.alpha1 + h*h) * df;
